01951cc6698928a845754da852cfada460bd737b
Finbarr Timbers <ft@finbarr.ca>
2014-07-06 10:28:53 -0600

    removed spelling from emacs

--- emacs-settings/custom-functions.el
+++ emacs-settings/custom-functions.el
@@ -62,23 +62,4 @@
   (add-to-list 'load-path (make-elget-path plugin)))
 
 
-(global-set-key (kbd "C-i") 'endless/ispell-word-then-abbrev)
-
-(defun endless/ispell-word-then-abbrev (p)
-  "Call `ispell-word'. Then create an abbrev for the correction made.
-With prefix P, create local abbrev. Otherwise it will be global."
-  (interactive "P")
-  (let ((bef (downcase (or (thing-at-point 'word) ""))) aft)
-    (call-interactively 'ispell-word)
-    (setq aft (downcase (or (thing-at-point 'word) "")))
-    (unless (string= aft bef)
-      (message "\"%s\" now expands to \"%s\" %sally"
-               bef aft (if p "loc" "glob"))
-      (define-abbrev
-        (if p global-abbrev-table local-abbrev-table)
-        bef aft))))
-
-(setq save-abbrevs t)
-(setq-default abbrev-mode t)
-
 (provide 'custom-functions)
